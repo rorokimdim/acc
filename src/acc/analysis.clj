8229e47cb34a1522f68f889bd14c18148f595c7a
Amit Shrestha <ashrestha@housecanary.com>
2017-12-08 16:36:42 -0800

    Handle analysis case for account without any investments.

--- src/acc/analysis.clj
+++ src/acc/analysis.clj
@@ -3,27 +3,29 @@
             [acc.time :as t]))
 
 (defn get-stats [numbers]
-  (let [sorted-numbers (sort numbers)
-        min-value (first sorted-numbers)
-        max-value (last sorted-numbers)
-        count-values (count numbers)
-        average-value (/ (apply + numbers) count-values)
-        mi (quot count-values 2)
-        median-value (if (even? count-values)
-                       (/ (+ (nth sorted-numbers mi)
-                             (nth sorted-numbers (dec mi)))
-                          2)
-                       (nth sorted-numbers mi))
-        square (fn [x] (* x x))
-        variance (/ (apply + (map #(square (- % average-value)) numbers))
-                    (dec count-values))
-        std-dev (Math/sqrt variance)]
-    {:min min-value
-     :max max-value
-     :count count-values
-     :average average-value
-     :median median-value
-     :std-dev std-dev}))
+  (if (empty? numbers)
+    {}
+    (let [sorted-numbers (sort numbers)
+          min-value (first sorted-numbers)
+          max-value (last sorted-numbers)
+          count-values (count numbers)
+          average-value (/ (apply + numbers) count-values)
+          mi (quot count-values 2)
+          median-value (if (even? count-values)
+                         (/ (+ (nth sorted-numbers mi)
+                               (nth sorted-numbers (dec mi)))
+                            2)
+                         (nth sorted-numbers mi))
+          square (fn [x] (* x x))
+          variance (/ (apply + (map #(square (- % average-value)) numbers))
+                      (dec count-values))
+          std-dev (Math/sqrt variance)]
+      {:min min-value
+       :max max-value
+       :count count-values
+       :average average-value
+       :median median-value
+       :std-dev std-dev})))
 
 (defn analyze
   "Analyzes rate of investment of a particular account."
@@ -37,8 +39,10 @@
                                             #(* (:amount %)
                                                 (t/date-str-to-years-past (:date %)))
                                             records))
-        total-gain-over-sum-of-investment-and-years (/ total-gain
-                                                       sum-of-investment-and-years)
+        total-gain-over-sum-of-investment-and-years (if (zero? sum-of-investment-and-years)
+                                                      0
+                                                      (/ total-gain
+                                                         sum-of-investment-and-years))
         analysis-table (for [m records]
                          (let [years (t/date-str-to-years-past (:date m))
                                initial-value (:amount m)
